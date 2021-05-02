a9780c7abafa2a75fe99bc8d752ac507ddf92e14
Tobias Falk <wieserfalke@gmail.com>
2021-05-02 12:35:39 +0200

    Signed-off-by: Tobias Falk <wieserfalke@gmail.com>

--- getAll.py
+++ getAll.py
@@ -12,4 +12,6 @@ def replace_in_file(file_path, search_text, new_text):
 this_readme = 'README.md'
 
 Repo.clone_from("https://github.com/tobiasfalk/Boxes-LC.git", "./boxes-lc/")
+replace_in_file(this_readme, '[1]: https://github.com/tobiasfalk/Boxes-LC/blob/master/img/70x210x40.png', '[1]: ./boxes/img/70x210x40.png')
+replace_in_file(this_readme, '[2]: https://github.com/tobiasfalk/Boxes-LC/blob/master/img/70x70x40.png', '[2]: ./boxes/img/70x70x40.png')
 replace_in_file(this_readme, '[3]: https://github.com/tobiasfalk/Boxes-LC/blob/master/README.md', '[3]: ./boxes/README.md')
